4d681c7800444ab2262bc57594c4c53812c98d2f
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-12-08 23:37:16 -0300

    Add files via upload

---
+++ clase4.js
@@ -0,0 +1,18 @@
+let CantidadProducto = parseInt(prompt("Ingrese la cantidad del producto"))
+const PrecioProducto = 50;
+
+for (let i = 1; i<=10;i++){
+  productos(CantidadProducto, PrecioProducto);
+
+}
+
+function productos() {
+  Compra = CantidadProducto * PrecioProducto;
+  if (CantidadProducto >= 20) {
+    Desc = Compra * 0.15;
+    Total = Compra - Desc;
+    console.log("el total de su compra es" + " " + Total);
+  } else if (CantidadProducto < 20) {
+    console.log("el total de su compra es" + " " + Compra);
+  }
+}
